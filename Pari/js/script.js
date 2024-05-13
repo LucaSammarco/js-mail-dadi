ffb5499e7d1ab516571d506e3df26ce7ffcff37c
Il tuo nome <la_tua_email@example.com>
2024-05-13 16:24:16 +0200

    created working script to check if even number

--- Pari/js/script.js
+++ Pari/js/script.js
@@ -7,5 +7,11 @@ const evenNum = [];
 for (let index = 0; index < 6; index++) {
     
     let userNumber = prompt("Insert number");
-    
-}
+    console.log(evenNum)
+    if (userNumber % 2 == 0) {
+        evenNum.push(userNumber)
+     
+} else {
+} }
+
+console.log(evenNum)
