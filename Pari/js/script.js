eff9668472e49b0b8709225c8b2750cb13eb744e
Il tuo nome <la_tua_email@example.com>
2024-05-13 16:15:30 +0200

    created promt loop

--- Pari/js/script.js
+++ Pari/js/script.js
@@ -1,36 +1,11 @@
-// const userName = prompt("Come ti chiami?");
-// console.log("Ciao " + userName);
-// console.log
+// Consegna 
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è pari inseriscilo nell’array
 
-// let number = 100
-// console.log (number);
-
-// const pi = 3.14
-// console.log (pi);
-
-// pari o dispari: chiedere all'utente/ssa se vuole pari o dispari e un numero intero compreso tra 1 e 9. Generare un numero casuale compreso tra 1 e 9, che sarà la giocata del computer. Stabilire e comunicare chi ha vinto secondo le regole di pari o dispari.
-
-// let choise = prompt ("Odd or even?")
-// let number = prompt ("Select a number")
-
-// let pcnum = Math.floor(Math.random () * 10);
-
-// console.log(pcnum)
+const evenNum = [];
 
 
-// let element = button
-
-// document.addEventListener("click", function(){
-//     document.getElementById("demo").innerHTML = "Hello World";
-//   });
-
-
-
-// > Chiedi all'utente 5 numeri, calcola e scrivi in console la loro somma, la loro media e un numero randomico tra 0 e la somma.
-
-let numero; ""
-let sum 0;
-
-for (let i = 0; i = 4; i++) {
-
+for (let index = 0; index < 6; index++) {
+    
+    let userNumber = prompt("Insert number");
+    
 }
