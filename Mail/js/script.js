424ae038bc1d2b903efa07edf8fd99348a9da773
Il tuo nome <la_tua_email@example.com>
2024-05-13 15:26:30 +0200

    email list setup

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,36 +1,10 @@
-// const userName = prompt("Come ti chiami?");
-// console.log("Ciao " + userName);
-// console.log
+const emails = [
+    "goku@gmail.com",
+    "vegeta@gmail.com",
+    "gohan@gmail.com",
+    "junior@gmail.com",
+    "trunks@gmail.com",
+    "crilin@gmail.com",
+    "freezer@gmail.com"
+]
 
-// let number = 100
-// console.log (number);
-
-// const pi = 3.14
-// console.log (pi);
-
-// pari o dispari: chiedere all'utente/ssa se vuole pari o dispari e un numero intero compreso tra 1 e 9. Generare un numero casuale compreso tra 1 e 9, che sarà la giocata del computer. Stabilire e comunicare chi ha vinto secondo le regole di pari o dispari.
-
-// let choise = prompt ("Odd or even?")
-// let number = prompt ("Select a number")
-
-// let pcnum = Math.floor(Math.random () * 10);
-
-// console.log(pcnum)
-
-
-// let element = button
-
-// document.addEventListener("click", function(){
-//     document.getElementById("demo").innerHTML = "Hello World";
-//   });
-
-
-
-// > Chiedi all'utente 5 numeri, calcola e scrivi in console la loro somma, la loro media e un numero randomico tra 0 e la somma.
-
-let numero; ""
-let sum 0;
-
-for (let i = 0; i = 4; i++) {
-
-}
